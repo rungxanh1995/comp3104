862d462d0fb92b330a748a5b29f6765630a0ca8d
Joe Pham <rungxanh2901@gmail.com>
2022-10-03 20:37:00 -0400

    Update new build

---
+++ build/precache-manifest.08c63a42d265f077d4afd0372ef4a0ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ba8c246914ab436f25d",
+    "url": "/rungxanh1995/comp3104/static/css/main.b8da3f6c.chunk.css"
+  },
+  {
+    "revision": "5ba8c246914ab436f25d",
+    "url": "/rungxanh1995/comp3104/static/js/main.5ba8c246.chunk.js"
+  },
+  {
+    "revision": "11dbaa4660685bee064b",
+    "url": "/rungxanh1995/comp3104/static/js/runtime~main.11dbaa46.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/rungxanh1995/comp3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "e3c5295a05d503b2260903b9b73f15f6",
+    "url": "/rungxanh1995/comp3104/index.html"
+  }
+];
